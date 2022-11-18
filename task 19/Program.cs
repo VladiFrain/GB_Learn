012a19bf841c7b5dbe354314cd05319af800bb68
VladiFrain <mail@vladifrain.ru>
2022-11-18 22:18:53 +0300

    Выполненные задания с 3 семинара

---
+++ task 19/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да*/
+
+Console.Write("Введите пятизначное число: ");
+var num = int.Parse(Console.ReadLine());
+
+if (num < 10000 || num > 100000)
+{
+    Console.WriteLine("Число не является пятизначным!");
+}
+else 
+{
+    int[] numbers = new int[5];
+    numbers[0] = num / 10000;
+    numbers[1] = num % 10000 / 1000;
+    numbers[2] = num % 1000 / 100;
+    numbers[3] = num % 100 / 10;
+    numbers[4] = num % 10;
+    
+    if (numbers[0] == numbers [4] && numbers[1] == numbers[3])
+    {
+        Console.WriteLine("Число является полиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является полиндромом");
+    }
+}
---
+++ task 21/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+using System;
+
+namespace Application
+{
+    class Point {
+        public Point(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;   
+            this.z = z;         
+        }
+        public int x;
+        public int y;
+        public int z;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ВВедите координату X первой точки: ");
+            var aX = int.Parse(Console.ReadLine());
+
+            Console.Write("ВВедите координату Y первой точки: ");
+            var aY = int.Parse(Console.ReadLine());
+
+            Console.Write("ВВедите координату Z первой точки: ");
+            var aZ = int.Parse(Console.ReadLine());
+
+            Console.Write("ВВедите координату X второй точки: ");
+            var bX = int.Parse(Console.ReadLine());
+
+            Console.Write("ВВедите координату Y второй точки: ");
+            var bY = int.Parse(Console.ReadLine());
+
+            Console.Write("ВВедите координату Z второй точки: ");
+            var bZ = int.Parse(Console.ReadLine());
+
+            var a = new Point(aX, aY, aZ);
+
+            var b = new Point(bX, bY, bZ);
+
+            var result = Math.Sqrt(Math.Pow((a.x-b.x), 2) + Math.Pow((a.y-b.y), 2) + Math.Pow((a.z-b.z), 2));
+
+            Console.WriteLine(Math.Round(result, 2));
+        }
+    }
+}
+
