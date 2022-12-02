91529e41f23dcf2f2bfffde7fefaf767f8fa07f7
VladiFrain <mail@vladifrain.ru>
2022-12-02 11:06:39 +0300

    Выполненные задания с 5 семинара

---
+++ task 36/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] arr = FillArray(12);
+PrintArray(arr);
+Console.WriteLine(GetSumOddElem(arr));
+
+int GetSumOddElem(int[] arr)
+{
+    int sum = 0;
+    foreach (var item in arr)
+    {
+        if (item % 2 != 0)
+        {
+            sum += item;
+        }
+    }
+
+    return sum;
+}
+
+int[] FillArray(int length) 
+{ 
+    int[] result = new int[length]; 
+    for(int i = 0; i < result.Length; i++) 
+    { 
+        result[i] = new Random().Next(-100, 100); 
+    } 
+    
+    return result; 
+} 
+
+void PrintArray(int[] arr)
+{ 
+    for(int i = 0; i < arr.Length; i++) 
+    { 
+        Console.Write($"{arr[i]} "); 
+    } 
+    Console.WriteLine();
+} 
---
+++ task 38/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] arr = FillArray(10);
+PrintArray(arr);
+Console.WriteLine(GetDiffMaxMin(arr));
+
+double GetDiffMaxMin(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+
+    return max - min;
+}
+
+double[] FillArray(int length) 
+{ 
+    double[] result = new double[length]; 
+    var rnd = new Random();
+    for(int i = 0; i < result.Length; i++) 
+    { 
+        result[i] = Math.Round(rnd.Next(0, 100) * rnd.NextDouble(), 2); 
+    } 
+    
+    return result; 
+} 
+
+void PrintArray(double[] arr)
+{ 
+    for(int i = 0; i < arr.Length; i++) 
+    { 
+        Console.Write($"{arr[i]} "); 
+    } 
+    Console.WriteLine();
+} 
