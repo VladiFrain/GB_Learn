4549dc7014220fad2d9270c9688592a6d614b610
VladiFrain <vladifrain@yandex.ru>
2023-03-24 11:18:20 +0300

    Выполненные задания к 6 семинару

---
+++ Java/seminar6/Ex.java
@@ -0,0 +1,10 @@
+package Java.seminar6;
+
+import Java.seminar6.model.Store;
+
+public class Ex {
+    public static void main(String[] args) {
+        Store store = new Store(5);
+        store.findLaptop();
+    }
+}
---
+++ Java/seminar6/model/Laptop.java
@@ -0,0 +1,15 @@
+package Java.seminar6.model;
+
+public class Laptop {
+    private Integer _ram;
+    private Integer _hd;
+    private String _os;
+    private String _color;
+
+    public Laptop(int ram, int hd, String os, String color) {
+        _ram = ram;
+        _hd = hd;
+        _os = os;
+        _color = color;
+    }
+}
